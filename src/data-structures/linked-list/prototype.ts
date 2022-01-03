bf2266fd742461f0bc845160d3384501a96e774d
gaowenbin <lotosv2010@163.com>
2022-01-03 18:19:59 +0800

    feat(prototype): 面试题二

--- src/data-structures/linked-list/prototype.ts
+++ src/data-structures/linked-list/prototype.ts
@@ -1,7 +1,6 @@
 export const instanceOf = (A: any, B: any): boolean => {
   let p: any = A;
   while(p) {
-    console.log(p.__proto__, B.prototype, 111)
     if(B.prototype === p) return true;
     p = p.__proto__;
   }
--- src/data-structures/linked-list/test/prototype.test.ts
+++ src/data-structures/linked-list/test/prototype.test.ts
@@ -9,4 +9,20 @@ describe("原型链 测试", function () {
     assert.equal(instanceOf({}, Object), true);
     assert.equal(instanceOf(1, Number), true);
   });
+  it('原型链', () => {
+    const foo: any = {};
+    const f: any = function(){};
+    (Object.prototype as any).a = 'value a';
+    (Function.prototype as any).b = 'value b';
+
+    // console.log(foo.a);
+    // console.log(foo.b);
+
+    // console.log(f.a);
+    // console.log(f.b);
+    expect(foo.a).to.be.equal('value a');
+    expect(foo.b).to.be.equal(undefined);
+    assert.equal(f.a, 'value a');
+    assert.equal(f.b, 'value b');
+  })
 });
