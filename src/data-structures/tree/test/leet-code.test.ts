2cdf051bd2b0d5b6e91e06e0409f64c9c173de9a
gaowenbin <lotosv2010@163.com>
2022-01-04 19:06:20 +0800

    feat(leetcode): 二叉树的最小深度

--- src/data-structures/tree/test/leet-code.test.ts
+++ src/data-structures/tree/test/leet-code.test.ts
@@ -1,6 +1,6 @@
 import "mocha";
 import { expect, assert } from "chai";
-import { maxDepth, maxDepth2 } from "../leet-code";
+import { maxDepth, maxDepth2, minDepth, minDepth2 } from "../leet-code";
 
 describe("二叉树的最大深度 测试", function () {
   const tree: any = {
@@ -23,5 +23,29 @@ describe("二叉树的最大深度 测试", function () {
   });
   it("二叉树的最大深度", function () {
     expect(maxDepth2(tree)).to.be.deep.equal(3);
+  });
+});
+
+describe("二叉树的最小深度 测试", function () {
+  const tree: any = {
+    val: 2,
+    right: {
+      val: 3,
+      right: {
+        val: 4,
+        right: {
+          val: 5,
+          right: {
+            val: 6
+          }
+        }
+      }
+    }
+  };
+  it("深度优先遍历", function () {
+    assert.equal(minDepth(tree), 5);
+  });
+  it("广度优先遍历", function () {
+    assert.equal(minDepth2(tree), 5);
   });
 });
