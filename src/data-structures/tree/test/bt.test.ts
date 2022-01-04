3eb044fb6163929ef17d5dffaafe21182d30fad0
gaowenbin <lotosv2010@163.com>
2022-01-04 17:23:01 +0800

    feat(binary tree): 二叉树(Binary Tree)遍历(非递归)

--- src/data-structures/tree/test/bt.test.ts
+++ src/data-structures/tree/test/bt.test.ts
@@ -1,6 +1,7 @@
 import "mocha";
 import { expect, assert } from "chai";
 import { preOrderTraversal, inOrderTraversal, postOrderTraversal } from "../bt";
+import { preOrderTraversal2, inOrderTraversal2, postOrderTraversal2 } from "../bt";
 const tree = {
   value: 1,
   left: {
@@ -37,5 +38,22 @@ describe("二叉树(Binary Tree)遍历(递归) 测试", function () {
     const nodeList: Array<any> = [];
     postOrderTraversal(tree, nodeList);
     expect(nodeList).to.be.deep.equal([4, 5, 2, 6, 7, 3, 1]);
+  });
+});
+describe("二叉树(Binary Tree)遍历(非递归) 测试", function () {
+  it("先序遍历", function () {
+    const nodeList: Array<any> = [];
+    preOrderTraversal2(tree, nodeList);
+    assert.deepEqual(nodeList, [1, 2, 4, 5, 3, 6, 7]);
+  });
+  it("中序遍历", function () {
+    const nodeList: Array<any> = [];
+    inOrderTraversal2(tree, nodeList);
+    assert.deepEqual(nodeList, [4, 2, 5, 1, 6, 3, 7]);
+  });
+  it("后序遍历", function () {
+    const nodeList: Array<any> = [];
+    postOrderTraversal2(tree, nodeList);
+    assert.deepEqual(nodeList, [4, 5, 2, 6, 7, 3, 1]);
   });
 });
