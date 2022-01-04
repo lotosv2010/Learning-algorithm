349d772da65cf2a4ffbfa59e9a0fc934a22f013e
gaowenbin <lotosv2010@163.com>
2022-01-04 16:22:53 +0800

    feat(binary tree): 二叉树(Binary Tree)遍历(递归)

--- src/data-structures/tree/bfs.ts
+++ src/data-structures/tree/bfs.ts
@@ -3,15 +3,15 @@
 // ● 把队头的 children 依次入队
 // ● 重复二、三步，直到队列为空
 export function breadthFirstSearch(tree: Record<string, any>, nodeList: Array<any>): Array<any> {
-  const queue = [];
+  const queue = []; // 新建一个队列
   if(tree) {
     queue.unshift(tree);
-    while(queue.length !== 0) {
-      const node = queue.shift(); // 头部
-      nodeList.push(node?.value);
+    while(queue.length !== 0) { // 重复二、三步
+      const node = queue.shift(); // 队头出队
+      nodeList.push(node?.value); // 访问队头
       const children: any = node?.children;
       for (let i = 0; i < children?.length; i++) {
-        queue.push(children[i]);
+        queue.push(children[i]); // 队头的 children 依次入对
       }
     }
   }
