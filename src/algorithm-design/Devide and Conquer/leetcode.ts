db56d34ad4a1df6bc8f2b2a161fda97d11e89663
gaowenbin <lotosv2010@163.com>
2022-01-08 21:22:46 +0800

    feat(leetcode): 相同的树

--- src/algorithm-design/Devide and Conquer/leetcode.ts
+++ src/algorithm-design/Devide and Conquer/leetcode.ts
@@ -64,3 +64,31 @@ export function invertTree(root: TreeNode | null): TreeNode | null {
     invertTree(root.left)
   );
 };
+
+// todo 相同的树
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     val: number
+ *     left: TreeNode | null
+ *     right: TreeNode | null
+ *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.left = (left===undefined ? null : left)
+ *         this.right = (right===undefined ? null : right)
+ *     }
+ * }
+ */
+
+export function isSameTree(p: TreeNode | null, q: TreeNode | null): boolean {
+  if(!p && !q) return true;
+  if(
+    p && q && // p 和 q 同时存在
+    p?.val === q?.val && // 根节点相同
+    isSameTree(p.left, q.left) && // 左子树相同
+    isSameTree(p.right, q.right) // 右子树相同
+  ) {
+    return true;
+  }
+  return false;
+};
