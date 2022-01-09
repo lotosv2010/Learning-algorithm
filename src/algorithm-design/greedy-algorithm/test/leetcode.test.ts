44eb238db697a08dfd1d0092a41e7ca6caa5787d
gaowenbin <lotosv2010@163.com>
2022-01-09 15:48:13 +0800

    feat(leetcode): 买卖股票的最佳时机 II

--- src/algorithm-design/greedy-algorithm/test/leetcode.test.ts
+++ src/algorithm-design/greedy-algorithm/test/leetcode.test.ts
@@ -1,6 +1,6 @@
 import { describe, it } from 'mocha';
 import { expect, assert } from 'chai';
-import { findContentChildren } from '../leetcode';
+import { findContentChildren, maxProfit } from '../leetcode';
 
 describe('贪心算法-分发饼干 测试', () => {
   it('分发饼干', () => {
@@ -8,5 +8,17 @@ describe('贪心算法-分发饼干 测试', () => {
   });
   it('分发饼干', () => {
     expect(findContentChildren([1, 2], [1, 2, 3])).to.be.equal(2);
+  });
+});
+
+describe('贪心算法-买卖股票的最佳时机-II 测试', () => {
+  it('买卖股票的最佳时机 II', () => {
+    expect(maxProfit([7,1,5,3,6,4])).to.be.equal(7);
+  });
+  it('买卖股票的最佳时机 II', () => {
+    expect(maxProfit([1,2,3,4,5])).to.be.equal(4);
+  });
+  it('买卖股票的最佳时机 II', () => {
+    expect(maxProfit([7,6,4,3,1])).to.be.equal(0);
   });
 });
