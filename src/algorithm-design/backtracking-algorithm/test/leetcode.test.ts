5d8c445da5b2a506a5781717a119a1e30e5c4f44
gaowenbin <lotosv2010@163.com>
2022-01-09 17:17:06 +0800

    feat(leetcode): 子集

--- src/algorithm-design/backtracking-algorithm/test/leetcode.test.ts
+++ src/algorithm-design/backtracking-algorithm/test/leetcode.test.ts
@@ -1,6 +1,6 @@
 import { describe, it } from 'mocha';
 import { expect, assert } from 'chai';
-import { permute } from '../leetcode';
+import { permute, subsets } from '../leetcode';
 
 describe('回溯算法-全排列 测试', () => {
   it('全排列', () => {
@@ -20,3 +20,15 @@ describe('回溯算法-全排列 测试', () => {
   });
 });
 
+describe('回溯算法-子集 测试', () => {
+  it('子集', () => {
+    const input = [1,2,3];
+    const res = [[],[1],[2],[3],[1,2],[1,3],[2,3],[1,2,3]];
+    expect(subsets(input)).to.be.deep.equal(res);
+  });
+  it('子集', () => {
+    const input = [0];
+    const res = [[],[0]];
+    expect(subsets(input)).to.be.deep.equal(res);
+  });
+});
