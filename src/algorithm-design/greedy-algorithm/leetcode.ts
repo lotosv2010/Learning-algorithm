80a38159eeb41f49e08d11c8046fc687f35d94c4
gaowenbin <lotosv2010@163.com>
2022-01-09 15:48:13 +0800

    feat(leetcode): 买卖股票的最佳时机 II

--- src/algorithm-design/greedy-algorithm/leetcode.ts
+++ src/algorithm-design/greedy-algorithm/leetcode.ts
@@ -14,3 +14,15 @@ export function findContentChildren(g: number[], s: number[]): number {
   return i;
 };
 
+// todo 买卖股票的最佳时机 II
+export function maxProfit(prices: number[]): number {
+  console.time('买卖股票的最佳时机 II');
+  let profit = 0;
+  for(let i = 1; i < prices.length; i++) {
+    if(prices[i - 1] < prices[i]) {
+      profit += prices[i] - prices[i - 1];
+    }
+  }
+  console.timeEnd('买卖股票的最佳时机 II');
+  return profit;
+};
