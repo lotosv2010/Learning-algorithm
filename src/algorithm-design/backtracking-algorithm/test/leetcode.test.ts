032b7bb871ca2b419421c6d5d258c610bdcdd95d
gaowenbin <lotosv2010@163.com>
2022-01-09 16:46:06 +0800

    feat(leetcode): 全排列

---
+++ src/algorithm-design/backtracking-algorithm/test/leetcode.test.ts
@@ -0,0 +1,22 @@
+import { describe, it } from 'mocha';
+import { expect, assert } from 'chai';
+import { permute } from '../leetcode';
+
+describe('回溯算法-全排列 测试', () => {
+  it('全排列', () => {
+    const input = [1,2,3];
+    const res = [[1,2,3],[1,3,2],[2,1,3],[2,3,1],[3,1,2],[3,2,1]];
+    expect(permute(input)).to.be.deep.equal(res);
+  });
+  it('全排列', () => {
+    const input = [0,1];
+    const res = [[0,1],[1,0]];
+    expect(permute(input)).to.be.deep.equal(res);
+  });
+  it('全排列', () => {
+    const input = [1];
+    const res = [[1]];
+    expect(permute(input)).to.be.deep.equal(res);
+  });
+});
+
