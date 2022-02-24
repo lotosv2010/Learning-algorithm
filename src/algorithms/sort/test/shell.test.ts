554843ece9a74cc0ceb6ebdfc81d8923411fd62b
gaowenbin <lotosv2010@163.com>
2022-02-24 13:54:50 +0800

    feat(shell sort): 希尔排序

---
+++ src/algorithms/sort/test/shell.test.ts
@@ -0,0 +1,17 @@
+import { describe, it } from 'mocha';
+import { expect } from 'chai';
+import { shell } from '../shell';
+import { defaultCompare, reverseCompare } from '../../../util';
+
+describe('希尔排序 测试', () => {
+  it('希尔排序-正序', () => {
+    const arr = [3, 44, 38, 5, 47, 15, 36, 26, 27, 2, 46, 4, 19, 50, 48];
+    const res = [2, 3, 4, 5, 15, 19, 26, 27, 36, 38, 44, 46, 47, 48, 50];
+    expect(shell(arr)).to.be.deep.equal(res);
+  });
+  it('希尔排序-倒序', () => {
+    const arr = [3, 44, 38, 5, 47, 15, 36, 26, 27, 2, 46, 4, 19, 50, 48];
+    const res = [50, 48, 47, 46, 44, 38, 36, 27, 26, 19, 15, 5, 4, 3, 2];
+    expect(shell(arr, reverseCompare(defaultCompare))).to.be.deep.equal(res);
+  });
+});
